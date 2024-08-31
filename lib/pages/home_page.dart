b668973687bc909e79fa45b1df2335efcf587be2
samir1120k <syraverma1120s@gmail.com>
2024-09-01 00:00:11 +0530

    day-12 Complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:test_2/Models/store.dart';
 import 'package:test_2/widgets/drawer.dart';
 import 'package:test_2/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData=jsonDecode(catalogJson);
+    var productsData=decodeData["products"];j
+  }
 
   @override
   Widget build(BuildContext context) {
